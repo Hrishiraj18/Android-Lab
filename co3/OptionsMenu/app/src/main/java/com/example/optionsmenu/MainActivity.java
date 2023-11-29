0d6f60b89e5fb12adae37cd5ff79e2ff21b6594e
Hrishiraj18 <121205156+Hrishiraj18@users.noreply.github.com>
2023-11-29 05:53:57 +0530

    new File Created

--- co3/OptionsMenu/app/src/main/java/com/example/optionsmenu/MainActivity.java
+++ co3/OptionsMenu/app/src/main/java/com/example/optionsmenu/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+       setSupportActionBar(toolbar);
     }
 
     @Override
